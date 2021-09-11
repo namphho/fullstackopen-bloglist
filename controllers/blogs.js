730de98e461f66f4ccd7464e8055c90c8baf6f69
Nam Pham <nam.phho@gmail.com>
2021-09-11 17:57:28 +0700

    write blog tc

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,36 @@
-const blogRouters = require('express').Router()
-const Blog = require('../models/blog')
+const blogRouters = require("express").Router();
+const Blog = require("../models/blog");
 
-blogRouters.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogRouters.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogRouters.post("/", (request, response) => {
+blogRouters.post("/", async (request, response) => {
   const blog = new Blog(request.body);
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+blogRouters.delete("/:id", async (request, response) => {
+  const id = request.params.id;
+  await Blog.findByIdAndRemove(id);
+  response.status(204).end();
+});
+
+blogRouters.put("/:id", async (request, response) => {
+  const body = request.body;
+
+  const updateData = {
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    updateData,
+    { new: true }
+  );
+  response.json(updatedBlog);
 });
 
 module.exports = blogRouters;
